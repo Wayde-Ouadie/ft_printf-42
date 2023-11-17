19426b6596464dbab1fe552016532d7be2c51598
Ouadie El Fengour <ouadie.xyz1@gmail.com>
2023-11-17 11:41:19 +0100

    Update

--- ft_printf.c
+++ ft_printf.c
@@ -6,11 +6,31 @@
 /*   By: oel-feng <oel-feng@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/16 20:35:47 by oel-feng          #+#    #+#             */
-/*   Updated: 2023/11/17 10:26:19 by oel-feng         ###   ########.fr       */
+/*   Updated: 2023/11/17 11:41:00 by oel-feng         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+
+static void	ft_recognition(va_list args, const char format, int *size)
+{
+	if (format == 'c')
+		ft_putchar(va_arg(args, int), size);
+	else if (format == 's')
+		ft_putstr(va_arg(args, char *), size);
+	else if (format == 'd' || format == 'i')
+		ft_putnbr(va_arg(args, int), size);
+	else if (format == 'u')
+		ft_putnbr_unsigned(va_arg(args, unsigned int), size);
+	else if (format == 'x')
+		ft_puthexlow(va_arg(args, unsigned int), size);
+	else if (format == 'X')
+		ft_puthexup(va_arg(args, unsigned int), size);
+	else if (format == 'p')
+		ft_putadress(va_arg(args, unsigned int), size);
+	else if (format == '%')
+		ft_putchar('%', size);
+}
 
 int	ft_printf(const char *format, ...)
 {
@@ -28,25 +48,9 @@ int	ft_printf(const char *format, ...)
 		if (format[i] == '%')
 		{
 			i++;
-			if (format[i])
-			{
-				if (format[i] == 'c')
-					ft_putchar(va_arg(args, int), &size);
-				else if (format[i] == 's')
-					ft_putstr(va_arg(args, char *), &size);
-				else if (format[i] == 'd' || format[i] == 'i')
-					ft_putnbr(va_arg(args, int), &size);
-				else if (format[i] == 'u')
-					ft_putnbr_unsigned(va_arg(args, unsigned int), &size);
-				else if (format[i] == 'x')
-					ft_puthexlow(va_arg(args, unsigned int), &size);
-				else if (format[i] == 'X')
-					ft_puthexup(va_arg(args, unsigned int), &size);
-				else if (format[i] == 'p')
-					ft_putadress(va_arg(args, unsigned int), &size);
-				else if (format[i] == '%')
-					ft_putchar('%', &size);
-			}
+			if (format[i] == '\0')
+				break ;
+			ft_recognition(args, format[i], &size);
 		}
 		else
 			ft_putchar(format[i], &size);
