f2d1c7b95d79f74b6f3a85c0126d8f924a10b23b
Ouadie El Fengour <ouadie.xyz1@gmail.com>
2023-11-21 13:50:15 +0100

    bonus

---
+++ ft_putnbr_unsigned_bonus.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr_unsigned.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oel-feng <oel-feng@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/17 05:22:17 by oel-feng          #+#    #+#             */
+/*   Updated: 2023/11/17 13:18:25 by oel-feng         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+void	ft_putnbr_unsigned(unsigned int nbr, int *size)
+{
+	if (nbr > 9)
+	{
+		ft_putnbr_unsigned(nbr / 10, size);
+		ft_putchar((nbr % 10) + 48, size);
+	}
+	else
+		ft_putchar(nbr + 48, size);
+}
